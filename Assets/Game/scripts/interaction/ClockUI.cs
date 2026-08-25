e045ce2cec8cf8635c5f7b8244536ecf229c6860
bitch <169540526+domeDiff@users.noreply.github.com>
2026-08-25 22:19:38 +0530

    clock_interaction

--- Assets/Game/scripts/interaction/ClockUI.cs
+++ Assets/Game/scripts/interaction/ClockUI.cs
@@ -1,17 +1,31 @@
 using UnityEngine;
+using System.Collections;
 using TMPro;
+using Unity.VisualScripting;
 
 public class ClockUI : MonoBehaviour
 {
     [SerializeField] private GameObject clockTimeText;
+    [SerializeField] private float displayDuration = 2f;
+
+    private Coroutine hideCoroutine;
 
     public void ShowTime()
     {
         clockTimeText.SetActive(true);
+
+        if(hideCoroutine != null)
+        {
+            StopCoroutine(hideCoroutine);
+        }
+
+        hideCoroutine = StartCoroutine(HideAfterDelay());
     }
 
-    public void HideTime()
+    private IEnumerator HideAfterDelay()
     {
+        yield return new WaitForSeconds(displayDuration);
         clockTimeText.SetActive(false);
+        hideCoroutine = null;
     }
 }
